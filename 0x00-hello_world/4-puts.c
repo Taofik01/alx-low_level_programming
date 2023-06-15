7135ba14961a00435cd7ca38bf4d7296716ef3d1
Taofik01 <Sulaimontaofik08@gmail.com>
2023-06-15 08:18:42 -0700

    4 done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - A pogram that prints a line with put
+ *
+ * Return: (0) (success)
+*/
 int main(void)
-{ 
+{
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
 
 
