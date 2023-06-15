1ceb251273eac6368238b3fe125878f952a6c3d1
Taofik01 <Sulaimontaofik08@gmail.com>
2023-06-15 08:48:50 -0700

    Adding 5th file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-/**
- * main - program that prints exactly using the printf function
- * return: 0 (success)
- */
 
+/**
+ * main - Entry point for the program
+ *
+ * Description: Prints a specific string using the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
-	return (0);
+printf("With proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
+
