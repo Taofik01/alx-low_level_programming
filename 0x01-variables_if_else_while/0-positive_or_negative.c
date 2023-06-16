5729e2b6681c928b013079fe4dbd62ecf3223f76
Taofik01 <Sulaimontaofik08@gmail.com>
2023-06-16 09:36:22 -0700

    0 task done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d number is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
@@ -23,7 +23,7 @@ printf("%d is zero\n", n);
 }
 else
 {
-printf("%d number is negative\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
