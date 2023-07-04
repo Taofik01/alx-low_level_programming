46f722ffae3d6a8864394aafc985a14b5d21fc5a
Taofik01 <Sulaimontaofik08@gmail.com>
2023-07-04 02:12:05 -0700

    first task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_memset - filling the memory block with a specific value
+*@s: starting address of memory to be filled
+*@b: the desired value
+*@n: number of bytes to be changed
+*
+* Return: Always 0
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i = 0;
+for (; n > 0; i++)
+{
+s[i] = b;
+}
+return (0);
+}
